30520b44dc36b4e581357f9bb2ab6c86e3f36209
kkavita92 <kkavita92@gmail.com>
2017-12-02 09:14:35 +0000

    Finish solution for Day 2 Part 1"

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,3 +1,9 @@
-def checksum(spreadsheet)
-  
-end
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../spreadsheet_input.txt", __FILE__)
+input = File.read(file_path)
+
+p input.split("\n")
+  .map{ |row| row.split("\t").map(&:to_i) }
+  .map { |row| row.max - row.min }
+  .reduce(:+)
