ebf9f309216ed196f6ed9ffaffe4f68f3dd1bdf2
Kavita Kalaichelvan <kkavita92@gmail.com>
2017-12-05 10:01:12 +0000

    Add solution for Day 5

---
+++ day5/day5_part2.rb
@@ -0,0 +1,20 @@
+file_path = File.expand_path("../input.txt", __FILE__)
+input = File.read(file_path)
+arr = input.split("\n").map(&:to_i)
+
+
+def jump(arr)
+  position = 0
+  counter = 0
+
+  until position > arr.length - 1
+    new_position = position + arr[position]
+    arr[position] >= 3 ? arr[position] -= 1 : arr[position] += 1
+    counter += 1
+    position = new_position
+  end
+
+counter
+end
+
+p jump(arr)
